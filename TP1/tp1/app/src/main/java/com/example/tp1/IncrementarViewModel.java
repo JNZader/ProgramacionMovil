89ff553de25e5688af6d982a7059797fb4aecc06
JNZader <javierzader@hotmail.com>
2024-11-20 01:13:36 -0300

    lei la consigna je
    uso de preincremento y de mutableLiveData

--- TP1/tp1/app/src/main/java/com/example/tp1/IncrementarViewModel.java
+++ TP1/tp1/app/src/main/java/com/example/tp1/IncrementarViewModel.java
@@ -1,15 +1,21 @@
 package com.example.tp1;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class IncrementarViewModel extends ViewModel {
-    private int contador=0;
+    private final MutableLiveData<Integer> contador = new MutableLiveData<>(0);
 
-    public void incrementar(){
-        contador=Incrementar.incrementar(contador);
-    }
-
-    public int getContador() {
+    public LiveData<Integer> getContador() {
         return contador;
     }
+
+    public void incrementar() {
+        if (contador.getValue() != null) {
+            int nuevoValor = Incrementar.incrementar(contador.getValue());
+            contador.setValue(nuevoValor);
+        }
+    }
+
 }
