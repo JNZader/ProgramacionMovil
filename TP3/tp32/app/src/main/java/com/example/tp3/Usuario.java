425a0592b79d1e05747525d047197a3bee4798ca
JNZader <javierzader@hotmail.com>
2024-11-28 08:58:18 -0300

    Create Usuario.java

---
+++ TP3/tp32/app/src/main/java/com/example/tp3/Usuario.java
@@ -0,0 +1,25 @@
+package com.example.tp3;
+
+public class Usuario {
+    private String nombre;
+    private int edad;
+
+    public Usuario(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Edad: " + edad;
+    }
+
+}
