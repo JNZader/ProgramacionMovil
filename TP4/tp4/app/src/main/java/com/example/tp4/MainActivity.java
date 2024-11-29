87a22444511699a058a44e5ab1fc617a3cc7c1c6
JNZader <javierzader@hotmail.com>
2024-11-29 04:26:30 -0300

    Update MainActivity.java

--- TP4/tp4/app/src/main/java/com/example/tp4/MainActivity.java
+++ TP4/tp4/app/src/main/java/com/example/tp4/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.tp4;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.tp4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private UsuarioViewModel usuarioViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        usuarioViewModel=new ViewModelProvider(this).get(UsuarioViewModel.class);
     }
 }
